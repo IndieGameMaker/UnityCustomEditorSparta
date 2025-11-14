41458f847298690803ab88cc5db7a49367efa2cc
Lee JaeHyun <myleje@gmail.com>
2025-11-14 17:21:50 +0900

    .

--- Assets/02_Scripts/Editor/GameManagerEditor.cs
+++ Assets/02_Scripts/Editor/GameManagerEditor.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 [CustomEditor(typeof(GameManager))]
 public class GameManagerEditor : Editor
 {
-    
+    public override void OnInspectorGUI()
+    {
+        var gameManager = (GameManager)target;
+
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("적 생성"))
+        {
+            
+        }
+        
+        if (GUILayout.Button("적 제거"))
+        {
+            
+        }
+        EditorGUILayout.EndHorizontal();
+    }
 }
